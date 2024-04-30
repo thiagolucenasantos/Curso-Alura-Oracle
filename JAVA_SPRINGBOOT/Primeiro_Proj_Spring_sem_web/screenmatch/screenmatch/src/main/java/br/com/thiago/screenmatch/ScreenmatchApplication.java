1b3f3c75233b0946f2f8f2cc9caa35c82b34005a
Thiago <tlucenalive@gmail.com>
2024-04-30 04:04:21 -0300

    Java Springboot

--- JAVA_SPRINGBOOT/Primeiro_Proj_Spring_sem_web/screenmatch/screenmatch/src/main/java/br/com/thiago/screenmatch/ScreenmatchApplication.java
+++ JAVA_SPRINGBOOT/Primeiro_Proj_Spring_sem_web/screenmatch/screenmatch/src/main/java/br/com/thiago/screenmatch/ScreenmatchApplication.java
@@ -1,11 +1,16 @@
 package br.com.thiago.screenmatch;
 
+import br.com.thiago.screenmatch.model.DadosEpisodios;
 import br.com.thiago.screenmatch.model.DadosSerie;
+import br.com.thiago.screenmatch.model.DadosTemporada;
 import br.com.thiago.screenmatch.service.ConsumoAPI;
 import br.com.thiago.screenmatch.service.ConverterDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A interface CommandLineRunner é um recurso poderoso dentro do universo do Spring Framework, amplamente utilizado no
@@ -28,5 +33,20 @@ public class ScreenmatchApplication implements CommandLineRunner {
 		ConverterDados conversor = new ConverterDados();
 		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
 		System.out.println(dados);
+
+		//Alterando o endereço da api para logo a baixo serem buscados os novos atributos da série
+		json = consumoApi.obterDados("https://www.omdbapi.com/?t=chicago+fire&season=2&episode=2&apikey=746a9683");
+		DadosEpisodios dadosEpisodios = conversor.obterDados(json, DadosEpisodios.class);
+		System.out.println(dadosEpisodios);
+
+		//Lista de Temporadas para serem impressas no console
+		List<DadosTemporada> temporadas = new ArrayList<>();
+
+		for (int i = 1; i<= dados.totalTemporadas(); i++){
+			json = consumoApi.obterDados("https://www.omdbapi.com/?t=chicago+fire&season=" + i + "&apikey=746a9683");
+			DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+			temporadas.add(dadosTemporada);
+		}
+		temporadas.forEach(System.out::println);
 	}
 }
